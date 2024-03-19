82b0aec50c66040d1f17accb2682ba3577b94900
Hafiz Shoaib Siddiq <159219257+student-shoaibsoomro@users.noreply.github.com>
2024-03-19 10:25:10 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+"use strict";
+//  Logical Operators:
+//  (And) It returns true if the both sides are true
+//  otherwise answer it returns False.
+Object.defineProperty(exports, "__esModule", { value: true });
+let age = 20;
+let income = 40000;
+console.log((age >= 18) && (income >= 30000));
+// (Or) It returns True if atleast one of the sides
+//  are true otherwise it returs False.
+let age1 = 25;
+let income1 = 20000;
+console.log((age >= 18) || (income >= 35000));
+// (Not) It returns the opposite Boolean value of the operand,
+// if the operand is true it returs false and if the operand is
+// false, It returns True.
+let a = 20;
+let b = 30;
+console.log(a < b);
